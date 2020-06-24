03284d0adcb3df53265554381d6204d192fa26ca
Łukasz Bazior <bandzior157@gmail.com>
2020-06-24 23:31:56 +0200

    Add SkillsViewController

---
+++ Mediaflex_CV_App/Mediaflex_CV_App/Scenes/Skills/SkillsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SkillsViewController.swift
+//  Mediaflex_CV_App
+//
+//  Created by Damian Tabański on 24/06/2020.
+//  Copyright © 2020 Łukasz Bazior. All rights reserved.
+//
+
+import UIKit
+
+class SkillsViewController: UIViewController {
+    
+    var tableView = UITableView()
+    
+}
---
+++ Mediaflex_CV_App/Mediaflex_CV_AppTests/Scenes/Skills/SkillsViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  SkillsViewControllerTests.swift
+//  Mediaflex_CV_AppTests
+//
+//  Created by Damian Tabański on 24/06/2020.
+//  Copyright © 2020 Łukasz Bazior. All rights reserved.
+//
+
+import XCTest
+@testable import Mediaflex_CV_App
+
+class SkillsViewControllerTests: XCTestCase {
+    
+    func test_tableViewRowsCount_equalsViewModelElementsCount() {
+        let sut = makeSUT()
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> SkillsViewController {
+        SkillsViewController()
+    }
+}
