440f52abe62f3af37d734b92c6f2ee171f7199d6
Łukasz Bazior <bandzior157@gmail.com>
2020-06-27 00:34:08 +0200

    Add ExperienceViewControlelr

---
+++ Mediaflex_CV_App/Mediaflex_CV_App/Scenes/Experience/ExperienceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ExperienceViewController.swift
+//  Mediaflex_CV_App
+//
+//  Created by Damian Tabański on 27/06/2020.
+//  Copyright © 2020 Łukasz Bazior. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceViewController: UIViewController {
+    
+}
---
+++ Mediaflex_CV_App/Mediaflex_CV_AppTests/Scenes/Experience/ExperienceViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  ExperienceViewControllerTests.swift
+//  Mediaflex_CV_AppTests
+//
+//  Created by Damian Tabański on 27/06/2020.
+//  Copyright © 2020 Łukasz Bazior. All rights reserved.
+//
+
+import XCTest
+@testable import Mediaflex_CV_App
+
+class ExperienceViewControllerTests: XCTestCase {
+    
+    func test_A() {
+        
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> ExperienceViewController {
+        ExperienceViewController()
+    }
+    
+}
