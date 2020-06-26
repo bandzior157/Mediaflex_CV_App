94c9808b8be045fc3052a97a0a6712cc1ab06db0
Łukasz Bazior <bandzior157@gmail.com>
2020-06-26 20:02:59 +0200

    Change fake resume service data

--- Mediaflex_CV_App/Mediaflex_CV_App/Services/FakeResumeService.swift
+++ Mediaflex_CV_App/Mediaflex_CV_App/Services/FakeResumeService.swift
@@ -11,9 +11,9 @@ class FakeResumeService {
     var delegate: ResumeServiceDelegate?
     
     private let resume = Resume(
-        name: "Fake name",
+        name: "Łukasz Bazior",
         imageUrl: "https://media-exp1.licdn.com/dms/image/C4D03AQF5fFNIFwTLLA/profile-displayphoto-shrink_200_200/0?e=1598486400&v=beta&t=DoVe9sBKssGRF01TyOX60xW754ABQ1fchbjhrOS1wXM",
-        role: "Fake role",
+        role: "iOS Developer",
         summary: "Some information about person. It could be summary of his pros and cons, experience, motivation or projects involed in. Should be visible in multiple lines.",
         email: "fake@gmail.com",
         phoneNumber: "+48507332167",
