fbd7e0861f495e9ae3555ffe27d57f9ba0bf7f3f
Łukasz Bazior <bandzior157@gmail.com>
2020-06-27 00:02:33 +0200

    Remove trailing separators in general table view

--- Mediaflex_CV_App/Mediaflex_CV_App/Scenes/General/GeneralViewController.swift
+++ Mediaflex_CV_App/Mediaflex_CV_App/Scenes/General/GeneralViewController.swift
@@ -37,6 +37,8 @@ class GeneralViewController: UIViewController {
         personView.backgroundColor = .systemTeal
         
         tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        tableView.removeTrailingSeparators()
+        
         layoutSubviews()
     }
     
