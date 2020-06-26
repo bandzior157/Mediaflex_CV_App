ccf6850aac9c88ea9b40ef0166cd943cb2ab8e27
Łukasz Bazior <bandzior157@gmail.com>
2020-06-26 17:59:18 +0200

    Change PhoneNumberHandler code organization

--- Mediaflex_CV_App/Mediaflex_CV_App/Other/PhoneNumberHandling/PhoneNumberHandler.swift
+++ Mediaflex_CV_App/Mediaflex_CV_App/Other/PhoneNumberHandling/PhoneNumberHandler.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class PhoneNumberHandler: PhoneNumberHandling {
+class PhoneNumberHandler {
     
     private let application: UIApplication
     
     init(application: UIApplication = UIApplication.shared) {
         self.application = application
     }
+    
+}
+
+extension PhoneNumberHandler: PhoneNumberHandling {
     
     func call(phoneNumber: String) throws {
         guard let url = URL(string: "tel://\(phoneNumber)") else {
