dbc1501e77b143e0cc418eb00a1d7d09339fe0a2
Łukasz Bazior <bandzior157@gmail.com>
2020-06-29 01:01:16 +0200

    Add animated deselection to experience cells

--- ResumeApp/ResumeApp/Scenes/Experience/ExperienceViewController.swift
+++ ResumeApp/ResumeApp/Scenes/Experience/ExperienceViewController.swift
@@ -85,6 +85,7 @@ extension ExperienceViewController: ExperienceViewing {
 extension ExperienceViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
         presenter?.didSelect(row: indexPath.row)
     }
     
