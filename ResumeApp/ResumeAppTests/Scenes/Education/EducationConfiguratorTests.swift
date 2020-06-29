9f711af38afdce4e1387c19a0ac35f4a747b5dea
Łukasz Bazior <bandzior157@gmail.com>
2020-06-29 22:40:22 +0200

    Add education configurator tests

---
+++ ResumeApp/ResumeAppTests/Scenes/Education/EducationConfiguratorTests.swift
@@ -0,0 +1,36 @@
+//
+//  EducationConfiguratorTests.swift
+//  ResumeAppTests
+//
+//  Created by Damian Tabański on 29/06/2020.
+//  Copyright © 2020 Łukasz Bazior. All rights reserved.
+//
+
+import XCTest
+@testable import ResumeApp
+
+class EducationConfiguratorTests: XCTestCase {
+    
+    private weak var weakViewController: EducationViewController?
+    private weak var weakPresenter: EducationPresenter?
+    
+    override func tearDown() {
+        super.tearDown()
+        XCTAssertNil(weakViewController)
+        XCTAssertNil(weakPresenter)
+    }
+    
+    func test_configure() {
+        let viewController = EducationViewController()
+        let presenter = EducationPresenter()
+        
+        weakViewController = viewController
+        weakPresenter = presenter
+
+        let sut = EducationConfigurator(viewController: viewController, presenter: presenter)
+        sut.configure()
+        
+        XCTAssertNotNil(presenter.view)
+    }
+    
+}
