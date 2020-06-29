f77a95fd8f684be1a475b5e391343dfe87007540
Łukasz Bazior <bandzior157@gmail.com>
2020-06-29 13:59:55 +0200

    Add header view to company experience detail view controller subviews

--- ResumeApp/ResumeApp/Scenes/CompanyExperienceDetail/CompanyExperienceDetailViewController.swift
+++ ResumeApp/ResumeApp/Scenes/CompanyExperienceDetail/CompanyExperienceDetailViewController.swift
@@ -10,12 +10,40 @@ import UIKit
 
 class CompanyExperienceDetailViewController: UIViewController {
     
-    private(set) var headerView = CompanyExperienceHeaderView()
+    private(set) lazy var headerView: CompanyExperienceHeaderView = {
+        let view = CompanyExperienceHeaderView()
+        view.backgroundColor = .systemTeal
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .orange
+        configureSubviews()
+    }
+    
+    private func configureSubviews() {
+        addSubviews()
+        layoutSubviews()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(headerView)
+    }
+    
+    private func layoutSubviews() {
+        let layoutGuide = view.safeAreaLayoutGuide
+        
+        let headerViewConstraints = [
+            headerView.topAnchor.constraint(equalTo: view.topAnchor),
+            headerView.leftAnchor.constraint(equalTo: layoutGuide.leftAnchor),
+            headerView.rightAnchor.constraint(equalTo: layoutGuide.rightAnchor),
+            headerView.bottomAnchor.constraint(lessThanOrEqualTo: layoutGuide.bottomAnchor)
+        ]
+        
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(headerViewConstraints)
     }
     
 }
@@ -25,7 +53,7 @@ extension CompanyExperienceDetailViewController: CompanyExperienceDetailViewing 
     
     func update(viewModel: CompanyExperienceDetailViewModel) {
         headerView.companyLabel.text = viewModel.companyName
-        headerView.companyLogo.setImage(at: viewModel.companyLogoUrlString)
+        headerView.imageView.setImage(at: viewModel.companyLogoUrlString)
         headerView.roleLabel.text = viewModel.role
     }
     
--- ResumeApp/ResumeApp/Views/CompanyExperienceHeaderView.swift
+++ ResumeApp/ResumeApp/Views/CompanyExperienceHeaderView.swift
@@ -10,8 +10,92 @@ import UIKit
 
 class CompanyExperienceHeaderView: UIView {
     
-    var companyLogo = UIImageView()
-    var companyLabel = UILabel()
-    var roleLabel = UILabel()
+    private static let imageViewSide: CGFloat = 120
+    
+    lazy var imageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.crop.circle")
+        imageView.tintColor = .gray
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = CompanyExperienceHeaderView.imageViewSide/2
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    lazy var companyLabel: UILabel = {
+        var label = UILabel()
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
+    
+    lazy var roleLabel: UILabel = {
+        var label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        nil
+    }
+    
+    private func setup() {
+        backgroundColor = .systemBackground
+        
+        placeholderSetup()
+        
+        let stackView = UIStackView(arrangedSubviews: [imageView, companyLabel, roleLabel])
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        
+        addSubview(stackView)
+        layoutImageView()
+        layout(contentView: stackView)
+    }
+    
+    private func placeholderSetup() {
+        companyLabel.text = "ABCDEFGHIJKLMNOPRS"
+        roleLabel.text = "ABCDEFGHIJK"
+        
+        setLabelsVisible(false)
+    }
+    
+    internal func setLabelsVisible(_ visible: Bool) {
+        let textColor : UIColor = visible ? .label : .clear
+        let backgroundColor: UIColor = visible ? .clear : .lightGray
+        companyLabel.textColor = textColor
+        roleLabel.textColor = textColor
+        
+        companyLabel.backgroundColor = backgroundColor
+        roleLabel.backgroundColor = backgroundColor
+    }
+    
+    
+    // MARK: - Layout
+    
+    private func layoutImageView() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(getImageViewConstraints())
+    }
+    
+    private func layout(contentView view: UIView) {
+        let constraints = NSLayoutConstraint.anchorConstraints(view: view, in: safeAreaLayoutGuide, margins: Margins(vertical: 32, horizontal: 16))
+                
+        view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    
+    // MARK: - Constraints
+    
+    private func getImageViewConstraints() -> [NSLayoutConstraint] {
+        [imageView.heightAnchor.constraint(equalToConstant: CompanyExperienceHeaderView.imageViewSide),
+        imageView.widthAnchor.constraint(equalToConstant: CompanyExperienceHeaderView.imageViewSide)]
+    }
     
 }
