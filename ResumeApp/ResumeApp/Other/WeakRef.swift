abb08df20d9dd68949778b828afdf70a9acb0755
Łukasz Bazior <bandzior157@gmail.com>
2020-06-29 21:40:58 +0200

    Add WeakRef

---
+++ ResumeApp/ResumeApp/Other/WeakRef.swift
@@ -0,0 +1,15 @@
+//
+//  WeakRef.swift
+//  ResumeApp
+//
+//  Created by Damian Tabański on 29/06/2020.
+//  Copyright © 2020 Łukasz Bazior. All rights reserved.
+//
+
+final class WeakRef<T: AnyObject> {
+    weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
