5d076abc3fc9235c30506a0729bc3305f4411771
Łukasz Bazior <bandzior157@gmail.com>
2020-06-29 20:42:15 +0200

    Remove unnecessary code

--- ResumeApp/ResumeApp/Views/CellViewModelUpdating/UITableViewCell+CellViewModelUpdating.swift
+++ ResumeApp/ResumeApp/Views/CellViewModelUpdating/UITableViewCell+CellViewModelUpdating.swift
@@ -19,57 +19,12 @@ extension UITableViewCell: CellViewModelUpdating {
         detailTextLabel?.textColor = .secondaryLabel
         detailTextLabel?.text = viewModel.subtitle
         
-        setImage(imageViewModel: viewModel.imageViewModel)
-        
+        imageView?.setImage(imageViewModel: viewModel.imageViewModel)
         imageView?.tintColor = .systemTeal
 
         selectionStyle = viewModel.selectable ? .default : .none
         
         accessoryType = viewModel.selectable ? .disclosureIndicator : .none
     }
-    
-    private func setImage(imageViewModel: ImageViewModel?) {
-        guard let imageViewModel = imageViewModel else {
-            imageView?.image = nil
-            return
-        }
-        
-        let size = CGSize(size: imageViewModel.size)
-        
-        switch imageViewModel.type {
-        case let .named(imageName):
-            setImage(named: imageName, size: size)
-        case let .url(urlString):
-            setImage(urlString: urlString, size: size)
-        }
-    }
-    
-    private func setImage(named imageName: String, size: CGSize) {
-        imageView?.image = UIImage(named: imageName)?
-        .resized(to: size)
-        .withRenderingMode(.alwaysTemplate)
-    }
-    
-    private func setImage(urlString: String, size: CGSize) {
-        configureImageViewAsPlaceholder(true)
 
-        imageView?.sd_setImage(with: URL(string: urlString), placeholderImage: getPlaceholderImage(size: size), completed: { [weak self] (image, _, _, _) in
-            ThreadGuarantee.guarantee(on: .main) {
-                guard let image = image else { return }
-                
-                self?.imageView?.image = image.resized(to: size)
-                self?.configureImageViewAsPlaceholder(false)
-            }
-        })
-    }
-    
-    private func getPlaceholderImage(size: CGSize) -> UIImage? {
-        UIImage(color: .secondarySystemBackground, size: size)
-    }
-    
-    private func configureImageViewAsPlaceholder(_ isPlaceholder: Bool) {
-        imageView?.layer.cornerRadius = isPlaceholder ? 8 : 0
-        imageView?.clipsToBounds = isPlaceholder
-    }
-    
 }
